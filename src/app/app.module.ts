88a2ed42dcf503d12dca7882aad46283fc905c6e
Wilmer A. Rubio Rojas <rubiorojasw@gmail.com>
2018-10-15 18:53:32 -0500

    Leyaout project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {  RouterModule, Routes } from '@angular/router';
+// External
+import {BreadcrumbsModule} from "ng6-breadcrumbs";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// App components
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '', component: HomeComponent,
+        data: {
+          breadcrumb: 'Home'
+        }
+      },
+      {
+        path: 'introduction', component: IntroductionComponent,
+        data: {
+          breadcrumb: 'Introducción'
+        }
+      }
+    ],
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    SidebarComponent,
+    IntroductionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    BreadcrumbsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
